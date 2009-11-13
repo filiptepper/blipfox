f837ce68fe45d7efdd5db3a136d43fdadd05f694
Filip Tepper <filip@tepper.pl>
2009-11-13 13:46:16 +0100

    version bump

--- BlipFox/content/js/BlipFox.js
+++ BlipFox/content/js/BlipFox.js
@@ -24,7 +24,7 @@
 const BLIPFOX_DEBUG = false;
 
 /* Wersja. */
-const BLIPFOX_VERSION = '1.1.5';
+const BLIPFOX_VERSION = '1.1.6';
 
 /* URL do API. */
 const BLIPFOX_API_URL = 'http://api.blip.pl/';
